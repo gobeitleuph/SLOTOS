36621aad1d628ea0693a85d04ff48099a460d95e
gobeitleuph <patrick.schwartz1998@gmail.com>
2022-03-19 00:24:29 +0100

    apfel

--- Microservice_Article/src/main/java/com/example/demo/MicroserviceArticleApplication.java
+++ Microservice_Article/src/main/java/com/example/demo/MicroserviceArticleApplication.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) // Nur solange keine Datenbankverbindung definiert ist
-//@EntityScan
 @SpringBootApplication
+@EnableJpaRepositories("com.example.*")
+@EntityScan("com.example.*")
 public class MicroserviceArticleApplication {
 
 	public static void main(String[] args) {
